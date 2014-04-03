065b508cf664a76530e30cc5a59244120e42ed29
Frederic Simon <freds@jfrog.com>
2014-04-03 12:26:35 -0700

    Wrong compare greater than

--- src/main/java/org/jfrog/hudson/plugins/artifactory/maven3extractor/MavenExtractorEnvironment.java
+++ src/main/java/org/jfrog/hudson/plugins/artifactory/maven3extractor/MavenExtractorEnvironment.java
@@ -122,7 +122,7 @@ public class MavenExtractorEnvironment extends Environment {
 
         buildStepCounter++; // Starts with zero and goes 0, 1, 2, ..
 
-        if(builders.size() >= buildStepCounter){ return; }
+        if(builders.size() <= buildStepCounter){ return; }
 
         Builder builder = builders.get( buildStepCounter );
 
